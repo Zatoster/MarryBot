c66074beabe562a6709ea97a75f6a3854cfdda95
Zatoster <Zatoster.mc@gmail.com>
2020-06-05 18:46:43 +0200

    Init

---
+++ Marry.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const   Discord  = require('discord.js')
+const   bot = new Discord.Client()
+
+//Toutes les actions à faire quand le bot se connecte
+bot.on("ready", function () {
+    console.log("Le bot est connecté");
+})
+bot.on('message', message => {
+    if (message.content.includes('victime')) {
+        message.channel.send('Press **F** to pay respect...')
+    }
+})
+
+// Utiliser un fichier .env à passer en gitignore pour garder le token du bot privé
+bot.login(process.env.BOT_ID);
