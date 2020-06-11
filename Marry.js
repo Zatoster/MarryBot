118f503607c91ff46f7371f4c7fea8dabb58d844
Zatoster <Zatoster.mc@gmail.com>
2020-06-11 13:55:32 +0200

    haha go brrrr

--- Marry.js
+++ Marry.js
@@ -5,6 +5,17 @@ const   bot = new Discord.Client()
 //Toutes les actions à faire quand le bot se connecte
 bot.on("ready", function () {
     console.log("Le bot est connecté");
+    bot.guilds.cache
+        .get("230405441845329930")
+        .members.cache.get("436998656491651072")
+        .setNickname("Belph goes brrrrrrr")
+        .then(
+            (res) => {},
+            (rej) => {
+                console.log(rej);
+            }
+        );
+
 })
 bot.on('message', message => {
     if (message.content.includes('victime')) {
@@ -19,7 +30,11 @@ bot.on('message', message => {
     if (message.content.includes('yaoi')) {
         message.channel.send('Non.');
     }
+    if (message.content.includes('brrr')) {
+        message.channel.send('Brrrrrrrrrrrrrrrrr');
+    }
 })
+
 
 // Utiliser un fichier .env à passer en gitignore pour garder le token du bot privé
 bot.login(process.env.BOT_ID);
